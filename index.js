6a0bb5e714a6504264e2b8e3815f0e92bfe710f5
mediaintegrati <mediaintegrati@gmail.com>
2018-02-20 11:30:31 +0100

    interval set

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-setInterval(function(){ wss.broadcast('request'); }, 8000);
+setInterval(function(){ wss.broadcast('request'); }, 1783);
 
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
