46f0ac172efd4c605434be658297c6feff9bbdd5
plcdnl <polaccodaniele@gmail.com>
2019-12-13 18:14:20 +0100

    reset

--- app/server.js
+++ app/server.js
@@ -13,8 +13,6 @@ console.log('Server listening at http://localhost:' + PORT);
 // serve static pages
 app.get('/', express.static(__dirname));
 
-app.get('/schiattone', express.static(__dirname)+'/schiattone');
-
 
 app.get('/getStatus', (req, res) => {
     return res.send({checkStatus});
